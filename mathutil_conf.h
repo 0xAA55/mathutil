525254d0fc146ddebca8e6b90ef102c70c1456dd
0xAA55 <0xaa55@0xaa55.com>
2019-09-24 11:44:08 +0900

    Adjust configuration between different detectable compilers

--- mathutil_conf.h
+++ mathutil_conf.h
@@ -3,9 +3,20 @@
 
 #if defined(_MSC_VER)
 #  define ALIGNED_(x) __declspec(align(x))
+#  ifndef MATHUTIL_VAR_NOT_ALIGNED
+#    define MATHUTIL_VAR_NOT_ALIGNED 0
+#  endif
 #  if _MSC_VER < 1910 || defined(_DEBUG)
 #    ifndef MATHUTIL_DETECT_CPU
 #      define MATHUTIL_DETECT_CPU 1
+#    endif
+#    ifndef MATHUTIL_REFONLY
+#      define MATHUTIL_REFONLY 0
+#    endif
+#  endif
+#  if _MSC_VER >= 1910
+#    ifndef MATHUTIL_DETECT_CPU
+#      define MATHUTIL_DETECT_CPU 0
 #    endif
 #    ifndef MATHUTIL_REFONLY
 #      define MATHUTIL_REFONLY 0
@@ -16,9 +27,12 @@
 #  ifndef MATHUTIL_DETECT_CPU
 #    define MATHUTIL_DETECT_CPU 0
 #  endif
-#    ifndef MATHUTIL_REFONLY
-#      define MATHUTIL_REFONLY 0
-#    endif
+#  ifndef MATHUTIL_REFONLY
+#    define MATHUTIL_REFONLY 0
+#  endif
+#  ifndef MATHUTIL_VAR_NOT_ALIGNED
+#    define MATHUTIL_VAR_NOT_ALIGNED 0
+#  endif
 #else
 #  define ALIGNED_(x)
 #  ifndef MATHUTIL_DETECT_CPU
@@ -27,9 +41,9 @@
 #  ifndef MATHUTIL_REFONLY
 #    define MATHUTIL_REFONLY 1
 #  endif
-#endif
-#ifndef MATHUTIL_VAR_NOT_ALIGNED
-#  define MATHUTIL_VAR_NOT_ALIGNED 1
+#  ifndef MATHUTIL_VAR_NOT_ALIGNED
+#    define MATHUTIL_VAR_NOT_ALIGNED 1
+#  endif
 #endif
 #ifndef MATHUTIL_VAR_ASSUME_ALIGNED
 #  define MATHUTIL_VAR_ASSUME_ALIGNED 0
